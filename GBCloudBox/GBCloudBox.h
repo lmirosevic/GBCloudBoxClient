5b632846c7dcb0d2e7e0733ddf030b3c7aebc75a
Luka Mirosevic <luka@goonbee.com>
2013-03-20 09:06:16 +0100

    Initial Commit

---
+++ GBCloudBox/GBCloudBox.h
@@ -0,0 +1,13 @@
+//
+//  GBCloudBox.h
+//  GBCloudBox
+//
+//  Created by Luka Mirosevic on 20/03/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBCloudBox : NSObject
+
+@end
